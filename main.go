2c25cf1be93bb24161ea22efc549e771bf7c7ed4
Liam <liam@eastcode.ca>
2024-10-12 18:00:53 -0300

    adding WSS (HTTPS served websites cannot use unsecure WS endpoints)

--- main.go
+++ main.go
@@ -255,11 +255,16 @@ func main() {
 
 	http.HandleFunc("/ws", server.handleConnections)
 
+	// Serve static files
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
 	go server.broadcastState() // Start broadcasting goroutine
 
-	fmt.Println("Server starting on :8080")
-	err := http.ListenAndServe(":8080", nil)
+	// Use TLS
+	fmt.Println("Server starting on :8443 (WSS)")
+	err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("ListenAndServeTLS: ", err)
 	}
 }
